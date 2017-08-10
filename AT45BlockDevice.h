25a87d80bb66b6c23a663452deae0a4063f60940
Jan Jongboom <janjongboom@gmail.com>
2017-08-10 17:18:03 +0200

    mbed OS 5 Block device for AT45 flash

---
+++ AT45BlockDevice.h
@@ -0,0 +1,153 @@
+#ifndef _FRAGMENTATION_FLASH_AT45_H_
+#define _FRAGMENTATION_FLASH_AT45_H_
+
+#include "mbed.h"
+#include "BlockDevice.h"
+#include "AT45.h"
+
+enum at45_bd_error {
+    BD_ERROR_NO_MEMORY          = -4002,
+};
+
+class AT45BlockDevice : public BlockDevice {
+public:
+    AT45BlockDevice() : spi(SPI_MOSI, SPI_MISO, SPI_SCK, SPI_NSS), at45(&spi, SPI_NSS) {
+        pagesize = at45.pagesize();
+        totalsize = pagesize * at45.pages();
+    }
+
+    ~AT45BlockDevice() {
+        if (pagebuffer) free(pagebuffer);
+    }
+
+    virtual int init() {
+        pagebuffer = (char*)calloc(pagesize, 1);
+        if (!pagebuffer) {
+            return BD_ERROR_NO_MEMORY;
+        }
+
+        return BD_ERROR_OK;
+    }
+
+    virtual int deinit() {
+        return BD_ERROR_OK;
+    }
+
+    virtual int program(const void *buffer, bd_addr_t addr, bd_size_t size) {
+        // Q: a 'global' pagebuffer makes this code not thread-safe...
+        // is this a problem? don't really wanna malloc/free in every call
+
+        // printf("write addr=%lu size=%d\n", addr, size);
+
+        // find the page
+        size_t bytes_left = size;
+        while (bytes_left > 0) {
+            uint32_t page = addr / pagesize; // this gets auto-rounded
+            uint32_t offset = addr % pagesize; // offset from the start of the pagebuffer
+            uint32_t length = pagesize - offset; // number of bytes to write in this pagebuffer
+            if (length > bytes_left) length = bytes_left; // don't overflow
+
+            // printf("Writing to page=%lu, offset=%lu, length=%lu\n", page, offset, length);
+
+            int r;
+
+            // retrieve the page first, as we don't want to overwrite the full page
+            r = at45.read_page(pagebuffer, page);
+            if (r != 0) return r;
+
+            // printf("pagebuffer of page %d is:\n", page);
+            // for (size_t ix = 0; ix < pagesize; ix++) {
+            //     printf("%02x ", pagebuffer[ix]);
+            // }
+            // printf("\n");
+
+            // now memcpy to the pagebuffer
+            memcpy(pagebuffer + offset, buffer, length);
+
+            // printf("pagebuffer after memcpy is:\n", page);
+            // for (size_t ix = 0; ix < pagesize; ix++) {
+            //     printf("%02x ", pagebuffer[ix]);
+            // }
+            // printf("\n");
+
+            // and write back
+            r = at45.write_page(pagebuffer, page);
+            if (r != 0) return r;
+
+            // change the page
+            bytes_left -= length;
+            addr += length;
+            buffer += length;
+        }
+
+        return BD_ERROR_OK;
+    }
+
+    virtual int read(void *buffer, bd_addr_t addr, bd_size_t size) {
+        // printf("read addr=%lu size=%d\n", addr, size);
+
+        size_t bytes_left = size;
+        while (bytes_left > 0) {
+            uint32_t page = addr / pagesize; // this gets auto-rounded
+            uint32_t offset = addr % pagesize; // offset from the start of the pagebuffer
+            uint32_t length = pagesize - offset; // number of bytes to read in this pagebuffer
+            if (length > bytes_left) length = bytes_left; // don't overflow
+
+            // printf("Reading from page=%lu, offset=%lu, length=%lu\n", page, offset, length);
+
+            int r = at45.read_page(pagebuffer, page);
+            if (r != 0) return r;
+
+            // copy into the provided buffer
+            memcpy(buffer, pagebuffer + offset, length);
+
+            // change the page
+            bytes_left -= length;
+            addr += length;
+            buffer += length;
+        }
+
+        return BD_ERROR_OK;
+    }
+
+    virtual int erase(bd_addr_t addr, bd_size_t size) {
+        // printf("erase addr=%lu size=%d\n", addr, size);
+
+        uint32_t start_page = addr / pagesize; // this gets auto-rounded
+        uint32_t end_page = (addr + size) / pagesize;
+
+        memset(pagebuffer, 0xff, pagesize);
+
+        for (size_t ix = start_page; ix <= end_page; ix++) {
+            int r = at45.write_page(pagebuffer, ix);
+            if (r != 0) return r;
+        }
+
+        return BD_ERROR_OK;
+    }
+
+    virtual bd_size_t get_read_size() const {
+        return pagesize;
+    }
+
+    virtual bd_size_t get_program_size() const {
+        return pagesize;
+    }
+
+    virtual bd_size_t get_erase_size() const {
+        return pagesize;
+    }
+
+    virtual bd_size_t size() const {
+        return totalsize;
+    }
+
+private:
+    SPI  spi;
+    AT45 at45;
+    bd_size_t pagesize;
+    bd_size_t totalsize;
+    char* pagebuffer;
+};
+
+#endif // _FRAGMENTATION_FLASH_AT45_H_
